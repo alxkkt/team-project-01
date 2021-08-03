13f1fbd84495ce8b48589cd05947c819b81b48d2
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2021-08-03 10:06:20 +0300

    reviews carousel added +scss

--- src/js/review-slider.js
+++ src/js/review-slider.js
@@ -1,30 +1,47 @@
-$(document).ready(function(){
-    $('.slider').slick(
+const ANIMATION_TIMING = 500;
+const $carouselEl = $('#carousel');
+const $reviewsEl = $('[data-reviews]');
+const $carouselRangeEl = $('[data-slider-range]');
+
+function renderCarouselRange(_, slick) {
+    $carouselRangeEl.text(`${slick.currentSlide + 1}/${slick.slideCount}`);
+}
+
+$carouselEl.on('init', renderCarouselRange);
+$carouselEl.slick({
+    centerMode: true,
+    arrows: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    focusOnSelect: true,
+    draggable: false,
+    autoplay: false,
+    speed: ANIMATION_TIMING,
+    responsive: [
         {
-            dots: true,
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            centerMode: true,
-            focusOnSelect: true,
-            asNavFor: '.text-slider',
-            responsive: [
-                {
-                  breakpoint: 767,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                  }
-                },
-              ]
+        breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+            }
         }
-    );
-    $('.text-slider').slick(
-        {
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            asNavFor: '.slider',
-        }
-    );
+    ]
+});
+
+$carouselEl.on('afterChange', renderCarouselRange);
+$carouselEl.on('beforeChange', (e, slick, currSlide, nextSlide) => {
+    if (currSlide === nextSlide) return;
+
+    $reviewsEl.addClass('reviews--animation');
+
+    $(`[data-slide]`).each((_, slideEl) => {
+        const $slideEl = $(slideEl);
+        const currentSlide = $slideEl.attr('data-slide');
+            
+        $slideEl.toggleClass('article-contant__text--active', +currentSlide === nextSlide);
+        setTimeout(() => {
+            $reviewsEl.removeClass('reviews--animation');
+        }, ANIMATION_TIMING);
+    });
 });
