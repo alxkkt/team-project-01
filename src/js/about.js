ff104a8291361149e51e4a78293913b2fa466b9f
Andrii Ivanichenko <ikenobiq@gmail.com>
2021-08-03 14:14:59 +0300

    Исправлены марджины у секций, добавлены и настроены кнопки

--- src/js/about.js
+++ src/js/about.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$('.about__button-more').click(function(){
-		$('.about__paragraph-last').toggleClass('hide');	
-		if ($('.about__paragraph-last').hasClass('hide')) {
+		$('.about__paragraph-last').toggleClass('about__hide');	
+		if ($('.about__paragraph-last').hasClass('about__hide')) {
 			$('.about__button-more').html('Less');
 		} else {
 			$('.about__button-more').html('Read more');
--- src/js/program.js
+++ src/js/program.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$('.program__button-more').click(function(){
-		$('.program__main-paragraph-last').toggleClass('hide');	
-		if ($('.program__main-paragraph-last').hasClass('hide')) {
+		$('.program__main-paragraph-last').toggleClass('program__hide');	
+		if ($('.program__main-paragraph-last').hasClass('program__hide')) {
 			$('.program__button-more').html('Less');
 		} else {
 			$('.program__button-more').html('Read more');
